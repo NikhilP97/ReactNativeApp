b2e0fe279a582ef4b5de761c2fcd152ee15454e7
Nikhil Pinto <nikhiljpinto@gmail.com>
2019-02-09 16:30:55 +0530

    changed radio button logic

--- src/components/QuizQuestionScreen.js
+++ src/components/QuizQuestionScreen.js
@@ -4,13 +4,11 @@ import {
   Text,
   ActivityIndicator,
   StyleSheet,
-  Picker,
   Button
 } from "react-native";
-import { Link } from "react-router-native";
 import QuestionData from "./QuestionData";
-import axios from "axios";
 import BackgroundView from './BackgroundView'
+import { RadioGroup, RadioButton } from "react-native-flexi-radio-button";
 
 export default class QuizQuestionScreen extends React.Component {
   constructor(props) {
@@ -18,10 +16,9 @@ export default class QuizQuestionScreen extends React.Component {
 
     this.state = {
       loading: false,
-      questions: [],
-      mLabQuestions: [],
+      questionsArray: [],
 
-      current: 0,
+      currentQuesNum: 0,
       correctScore: 5,
       totalScore: 50,
 
@@ -40,31 +37,28 @@ export default class QuizQuestionScreen extends React.Component {
     const response = await fetch(
       `https://wt-0cd1e9e1874510cd90a9ec9f1e085110-0.sandbox.auth0-extend.com/express-with-mongo-db/5c5c77a6e7179a2ed61138aa`
     );
-    console.log("response", response);
-    const questions = await response.json();
-    console.log("questions", questions);
-    console.log("questions 0 ", questions.results);
-    const  results  = questions.results;
-    console.log("results", results);
+    // console.log("response", response);
+    const getQuestions = await response.json();
+    // console.log("getQuestions 0 ", getQuestions.results);
+    const  setQuestionArray  = getQuestions.results;
+    // console.log("results", results);
 
-    results.forEach(item => {
+    setQuestionArray.forEach(item => {
       item.id = Math.floor(Math.random() * 10000);
     });
 
-    console.log("results: ",results);
+    console.log(" Questions Array: ",setQuestionArray);
 
-    await this.setState({ questions: results, loading: false});
+    await this.setState({ questionsArray: setQuestionArray, loading: false});
 
-    console.log("mLabQuestions again",this.state.mLabQuestions);
 
   };
 
   reset = () => {
     this.setState(
       {
-        questions: [],
-        mLabQuestions: [],
-        current: 0,
+        questionsArray: [],
+        currentQuesNum: 0,
         results: {
           score: 0,
           correctAnswers: 0
@@ -78,7 +72,7 @@ export default class QuizQuestionScreen extends React.Component {
   };
 
   submitAnswer = (index, answer) => {
-    const question = this.state.questions[index];
+    const question = this.state.questionsArray[index];
     const isCorrect = question.correct_answer === answer;
     const results = { ...this.state.results };
 
@@ -88,7 +82,7 @@ export default class QuizQuestionScreen extends React.Component {
       : results.correctAnswers;
 
     this.setState({
-      current: index + 1,
+      currentQuesNum: index + 1,
       results,
       completed: index === 9 ? true : false
     });
@@ -99,7 +93,41 @@ export default class QuizQuestionScreen extends React.Component {
     this.fetchQuestions();
   }
 
+
+  onSelect = (index, value, quesNum) => {
+    console.log("in on select");
+    console.log("state:",this.state);
+    this.setState({
+      text: `Selected index: ${index} , value: ${value}`
+    })
+    const question = this.state.questionsArray[quesNum];
+    console.log("questionONSelect:", question);
+    const isCorrect = question.correct_answer === value;
+    const results = { ...this.state.results };
+
+    console.log("isCorrect", isCorrect);
+    console.log("INON results:", results)
+
+    results.score = isCorrect ? results.score + 5 : results.score;
+    results.correctAnswers = isCorrect
+      ? results.correctAnswers + 1
+      : results.correctAnswers;
+
+    this.setState({results});
+
+  }
+
+  getNextQuestion = () => {
+    var index = this.state.currentQuesNum;
+    this.setState({
+      currentQuesNum: index + 1,
+      completed: index === 9 ? true : false
+    });
+  }
+
+
   render() {
+
     return (
       <BackgroundView>
       <View style={styles.container}>
@@ -110,16 +138,49 @@ export default class QuizQuestionScreen extends React.Component {
           </View>
         )}
 
-        {!!this.state.questions.length > 0 &&
+        {!!this.state.questionsArray.length > 0 &&
           this.state.completed === false && (
-            <QuestionData
-              onSelect={answer => {
-                this.submitAnswer(this.state.current, answer);
-              }}
-              question={this.state.questions[this.state.current]}
-              correctPosition={Math.floor(Math.random() * 3)}
-              current={this.state.current}
-            />
+            <View style={styles.container}>
+              
+              <Text style={{ fontSize: 16, color: "#666", textAlign: "right" }}>
+                {this.state.currentQuesNum + 1} out of 10
+              </Text>
+
+              <Text style={{ fontSize: 32, fontWeight: "bold", color: "#0d87a1" }}>
+                {this.state.questionsArray[this.state.currentQuesNum].question}
+              </Text>
+
+
+
+              <RadioGroup
+                onSelect = {(index, value) => this.onSelect(index, value, this.state.currentQuesNum)}
+              >
+                <RadioButton value={this.state.questionsArray[this.state.currentQuesNum].correct_answer} >
+                  <Text>{this.state.questionsArray[this.state.currentQuesNum].correct_answer}</Text>
+                </RadioButton>
+         
+                <RadioButton value={this.state.questionsArray[this.state.currentQuesNum].incorrect_answers[0]}>
+                  <Text>{this.state.questionsArray[this.state.currentQuesNum].incorrect_answers[0]}</Text>
+                </RadioButton>
+         
+                <RadioButton value={this.state.questionsArray[this.state.currentQuesNum].incorrect_answers[1]}>
+                  <Text>{this.state.questionsArray[this.state.currentQuesNum].incorrect_answers[1]}</Text>
+                </RadioButton>
+
+                <RadioButton value={this.state.questionsArray[this.state.currentQuesNum].incorrect_answers[2]}>
+                  <Text>{this.state.questionsArray[this.state.currentQuesNum].incorrect_answers[2]}</Text>
+                </RadioButton>
+
+              </RadioGroup>
+
+              <Button
+                title="Submit Answser"
+                onPress={this.getNextQuestion}
+              />
+              
+              <Text style={styles.text}>{this.state.text}</Text>
+      
+            </View>
           )}
 
         <View
