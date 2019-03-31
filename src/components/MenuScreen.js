714256d0cb49b27e3b515e5c2a37425f07e03cad
Nikhil Pinto <nikhiljpinto@gmail.com>
2019-03-31 22:35:40 +0530

    added 6 new cards

--- src/components/MenuScreen.js
+++ src/components/MenuScreen.js
@@ -4,7 +4,7 @@ import {
 	Dimensions,
 	View
 } from 'react-native';
-import { CardViewWithIcon } from 'react-native-simple-card-view';
+import { CardViewWithIcon, CardViewWithImage } from 'react-native-simple-card-view';
 import {Actions, ActionConst} from 'react-native-router-flux';
 import BackgroundView from './BackgroundView'
 
@@ -15,10 +15,10 @@ export default class MenuScreen extends Component {
 			shadowOffsetWidth : 2,
 			shadowOffsetHeight: 2,
 			shadowOpacity     : 0.1,
-			hadowRadius       : 5,
+			shadowRadius       : 5,
 			bgColor           : '#fff',
 			padding           : 5,
-			margin            : 10,
+			margin            : 15,
 			borderRadius      : 3,
 			elevation         : 3,
 			width             : (Dimensions.get("window").width / 2) - 30
@@ -27,30 +27,105 @@ export default class MenuScreen extends Component {
 	    	<BackgroundView>
 	    	<View style={ styles.container }>
 		        <View style={ {alignItems   : "center",flexDirection: "row",flexWrap     : 'wrap',}}>
-					<CardViewWithIcon
-					withBackground={ false }
-					androidIcon={ 'logo-github' }
-					iosIcon={ 'logo-github' }
-					iconHeight={ 30 }
-					iconColor={ '#333' }
-					title={ 'GITHUB' }
-					contentFontSize={ 10 }
-					titleFontSize={ 12 }
-					style={ miniCardStyle }
-					onPress={ () => Actions.quizStartScreen()}
-					/>
-					<CardViewWithIcon
-					withBackground={ false }
-					androidIcon={ 'logo-youtube' }
-					iosIcon={ 'logo-youtube' }
-					iconHeight={ 30 }
-					iconColor={ '#ff0000' }
-					title={ 'YOUTUBE' }
-					contentFontSize={ 10 }
-					titleFontSize={ 12 }
-					style={ miniCardStyle }
-					onPress={ () => Actions.quizStartScreen()}
-					/>
+					
+					<CardViewWithImage
+				        width={ 175}
+				        source={ require('../images/menuLogos/java1.png') }
+				        title={ 'Java' }
+				        titleFontSize={17}
+				        imageWidth={ 40 }
+				        imageHeight={ 40 }
+				        onPress={() => Actions.quizStartScreen()}
+				        roundedImage={ false }
+				        roundedImageValue={ 50 }
+				        style={ miniCardStyle }
+				        imageMargin={ {top: 5} }
+				    />
+				    <CardViewWithImage
+				        width={ 175}
+				        source={ require('../images/menuLogos/aptitude1.jpg') }
+				        title={ 'Aptitude' }
+				        titleFontSize={17}
+				        imageWidth={ 40 }
+				        imageHeight={ 40 }
+				        onPress={() => Actions.quizStartScreen()}
+				        roundedImage={ false }
+				        roundedImageValue={ 50 }
+				        style={ miniCardStyle }
+				        imageMargin={ {top: 5} }
+				    />
+				    <CardViewWithImage
+				        width={ 175}
+				        source={ require('../images/menuLogos/c1.png') }
+				        imageWidth={ 82 }
+				        imageHeight={ 82 }
+				        onPress={() => Actions.quizStartScreen()}
+				        roundedImage={ false }
+				        roundedImageValue={ 50 }
+				        style={ miniCardStyle }
+				        imageMargin={ {top: 5} }
+				    />
+				    <CardViewWithImage
+				        width={ 175}
+				        source={ require('../images/menuLogos/cpp1.png') }
+				        imageWidth={ 82 }
+				        imageHeight={ 82 }
+				        onPress={() => Actions.quizStartScreen()}
+				        roundedImage={ false }
+				        roundedImageValue={ 50 }
+				        style={ miniCardStyle }
+				        imageMargin={ {top: 5} }
+				    />
+				    <CardViewWithImage
+				        width={ 175}
+				        source={ require('../images/menuLogos/python1.png') }
+				        imageWidth={ 43 }
+				        imageHeight={ 43 }
+				        title={ 'Python' }
+				        titleFontSize={17}
+				        onPress={() => Actions.quizStartScreen()}
+				        roundedImage={ false }
+				        roundedImageValue={ 50 }
+				        style={ miniCardStyle }
+				        
+				    />
+				    <CardViewWithImage
+				        width={ 175}
+				        source={ require('../images/menuLogos/ds1.png') }
+				        imageWidth={ 43 }
+				        imageHeight={ 43 }
+				        title={ 'Data Structures' }
+				        titleFontSize={17}
+				        onPress={() => Actions.quizStartScreen()}
+				        roundedImage={ false }
+				        roundedImageValue={ 50 }
+				        style={ miniCardStyle }
+				    />
+				    <CardViewWithImage
+				        width={ 175}
+				        source={ require('../images/menuLogos/algo1.png') }
+				        imageWidth={ 43 }
+				        imageHeight={ 43 }
+				        title={ 'Algorithms' }
+				        titleFontSize={17}
+				        onPress={() => Actions.quizStartScreen()}
+				        roundedImage={ false }
+				        roundedImageValue={ 50 }
+				        style={ miniCardStyle }
+				    />
+				    <CardViewWithImage
+				        width={ 175}
+				        source={ require('../images/menuLogos/dbms2.png') }
+				        imageWidth={ 38 }
+				        imageHeight={ 38 }
+				        title={ 'DBMS' }
+				        titleFontSize={17}
+				        onPress={() => Actions.quizStartScreen()}
+				        roundedImage={ false }
+				        roundedImageValue={ 50 }
+				        style={ miniCardStyle }
+				        imageMargin={ {top: 5} }
+				    />
 		        </View>
 	     	</View>
 	     	</BackgroundView>
